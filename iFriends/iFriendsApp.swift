faefb410b6160622aa3504cd50f3a1ced03086ef
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-24 06:38:37 +0100

    Initial Commit

---
+++ iFriends/iFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFriendsApp.swift
+//  iFriends
+//
+//  Created by Philipp Sanktjohanser on 24.12.22.
+//
+
+import SwiftUI
+
+@main
+struct iFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
